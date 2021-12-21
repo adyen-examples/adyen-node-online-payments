e845bb2f7e2e109cd46899ba6e47805447ce3a2e
gcatanese <gcatanese@yahoo.com>
2021-12-21 10:43:19 +0100

    Remove unused code, add getPort() function

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post("/api/sessions", async (req, res) => {
       countryCode: "NL",
       merchantAccount: process.env.MERCHANT_ACCOUNT, // required
       reference: orderRef, // required: your Payment Reference
-      returnUrl: `http://localhost:8080/api/handleShopperRedirect?orderRef=${orderRef}` // set redirect URL required for some payment methods
+      returnUrl: `http://localhost:${getPort()}/api/handleShopperRedirect?orderRef=${orderRef}` // set redirect URL required for some payment methods
     });
 
     res.json(response);
@@ -66,110 +66,6 @@ app.post("/api/sessions", async (req, res) => {
 });
 
 
-// Get payment methods
-app.post("/api/getPaymentMethods", async (req, res) => {
-  try {
-    const response = await checkout.paymentMethods({
-      channel: "Web",
-      merchantAccount: process.env.MERCHANT_ACCOUNT,
-    });
-    res.json(response);
-  } catch (err) {
-    console.error(`Error: ${err.message}, error code: ${err.errorCode}`);
-    res.status(err.statusCode).json(err.message);
-  }
-});
-
-// Submitting a payment
-app.post("/api/initiatePayment", async (req, res) => {
-  const currency = findCurrency(req.body.paymentMethod.type);
-  // find shopper IP from request
-  const shopperIP = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-  try {
-    // unique ref for the transaction
-    const orderRef = uuid();
-    // Ideally the data passed here should be computed based on business logic
-    const response = await checkout.payments({
-      amount: { currency, value: 1000 }, // value is 10€ in minor units
-      reference: orderRef, // required
-      merchantAccount: process.env.MERCHANT_ACCOUNT, // required
-      channel: "Web", // required
-      additionalData: {
-        // required for 3ds2 native flow
-        allow3DS2: true,
-      },
-      origin: "http://localhost:8080", // required for 3ds2 native flow
-      browserInfo: req.body.browserInfo, // required for 3ds2
-      shopperIP, // required by some issuers for 3ds2
-      returnUrl: `http://localhost:8080/api/handleShopperRedirect?orderRef=${orderRef}`, // required for 3ds2 redirect flow
-      // special handling for boleto
-      paymentMethod: req.body.paymentMethod.type.includes("boleto")
-        ? {
-            type: "boletobancario_santander",
-          }
-        : req.body.paymentMethod,
-      // Below fields are required for Boleto:
-      socialSecurityNumber: req.body.socialSecurityNumber,
-      shopperName: req.body.shopperName,
-      billingAddress:
-        typeof req.body.billingAddress === "undefined" || Object.keys(req.body.billingAddress).length === 0
-          ? null
-          : req.body.billingAddress,
-      deliveryDate: "2023-12-31T23:00:00.000Z",
-      shopperStatement: "Aceitar o pagamento até 15 dias após o vencimento.Não cobrar juros. Não aceitar o pagamento com cheque",
-      // Below fields are required for Klarna:
-      countryCode: req.body.paymentMethod.type.includes("klarna") ? "DE" : null,
-      shopperReference: "12345",
-      shopperEmail: "youremail@email.com",
-      shopperLocale: "en_US",
-      lineItems: [
-        {
-          quantity: "1",
-          amountExcludingTax: "331",
-          taxPercentage: "2100",
-          description: "Shoes",
-          id: "Item 1",
-          taxAmount: "69",
-          amountIncludingTax: "400",
-        },
-        {
-          quantity: "2",
-          amountExcludingTax: "248",
-          taxPercentage: "2100",
-          description: "Socks",
-          id: "Item 2",
-          taxAmount: "52",
-          amountIncludingTax: "300",
-        },
-      ],
-    });
-
-    res.json(response);
-  } catch (err) {
-    console.error(`Error: ${err.message}, error code: ${err.errorCode}`);
-    res.status(err.statusCode).json(err.message);
-  }
-});
-
-app.post("/api/submitAdditionalDetails", async (req, res) => {
-  // Create the payload for submitting payment details
-  const payload = {
-    details: req.body.details,
-    paymentData: req.body.paymentData,
-  };
-
-  try {
-    // Return the response back to client
-    // (for further action handling or presenting result to shopper)
-    const response = await checkout.paymentsDetails(payload);
-
-    res.json(response);
-  } catch (err) {
-    console.error(`Error: ${err.message}, error code: ${err.errorCode}`);
-    res.status(err.statusCode).json(err.message);
-  }
-});
 
 // Handle all redirects from payment type
 app.all("/api/handleShopperRedirect", async (req, res) => {
@@ -240,25 +136,12 @@ app.get("/result/:type", (req, res) =>
 
 /* ################# UTILS ###################### */
 
-function findCurrency(type) {
-  switch (type) {
-    case "ach":
-      return "USD";
-    case "wechatpayqr":
-    case "alipay":
-      return "CNY";
-    case "dotpay":
-      return "PLN";
-    case "boletobancario":
-    case "boletobancario_santander":
-      return "BRL";
-    default:
-      return "EUR";
-  }
+function getPort() {
+  return process.env.PORT || 8080;
 }
 
 /* ################# end UTILS ###################### */
 
 // Start server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server started -> http://localhost:${PORT}`));
+app.listen(getPort(), () => console.log(`Server started -> http://localhost:${getPort()}`));
