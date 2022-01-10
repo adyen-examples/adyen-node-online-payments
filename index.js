9a830c2e57013814ac33dda3c77d76c403b0033f
Beppe Catanese <1771700+gcatanese@users.noreply.github.com>
2022-01-10 14:20:33 +0100

    14 sessions endpoint (#15)
    * Upgrade Adyen library
    
    * Start and complete checkout using Sessions endpoint
    
    * Use platform env variable (instead of hardcoding test|live)
    
    * Use platform env variable (instead of hardcoding test|live)
    
    * Add /api/sessions endpoint
    
    * Remove unused code, add getPort() function
    
    * Remove platform env variable (hardcode 'test' instead)
    
    * Add webhook
    
    * Verify HMAC signature
    
    * Log merchantReference and eventCode
    
    * Add HMAC and tidy up README

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const hbs = require("express-handlebars");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const { uuid } = require("uuidv4");
+const { hmacValidator } = require('@adyen/api-library');
 const { Client, Config, CheckoutAPI } = require("@adyen/api-library");
 
 // init app
@@ -27,7 +28,7 @@ dotenv.config({
 const config = new Config();
 config.apiKey = process.env.API_KEY;
 const client = new Client({ config });
-client.setEnvironment("TEST");
+client.setEnvironment("TEST");  // change to LIVE for production
 const checkout = new CheckoutAPI(client);
 
 app.engine(
@@ -43,83 +44,19 @@ app.set("view engine", "handlebars");
 
 /* ################# API ENDPOINTS ###################### */
 
-// Get payment methods
-app.post("/api/getPaymentMethods", async (req, res) => {
-  try {
-    const response = await checkout.paymentMethods({
-      channel: "Web",
-      merchantAccount: process.env.MERCHANT_ACCOUNT,
-    });
-    res.json(response);
-  } catch (err) {
-    console.error(`Error: ${err.message}, error code: ${err.errorCode}`);
-    res.status(err.statusCode).json(err.message);
-  }
-});
-
-// Submitting a payment
-app.post("/api/initiatePayment", async (req, res) => {
-  const currency = findCurrency(req.body.paymentMethod.type);
-  // find shopper IP from request
-  const shopperIP = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+// Invoke /sessions endpoint
+app.post("/api/sessions", async (req, res) => {
 
   try {
     // unique ref for the transaction
     const orderRef = uuid();
     // Ideally the data passed here should be computed based on business logic
-    const response = await checkout.payments({
-      amount: { currency, value: 1000 }, // value is 10€ in minor units
-      reference: orderRef, // required
+    const response = await checkout.sessions({
+      amount: { currency: "EUR", value: 1000 }, // value is 10€ in minor units
+      countryCode: "NL",
       merchantAccount: process.env.MERCHANT_ACCOUNT, // required
-      channel: "Web", // required
-      additionalData: {
-        // required for 3ds2 native flow
-        allow3DS2: true,
-      },
-      origin: "http://localhost:8080", // required for 3ds2 native flow
-      browserInfo: req.body.browserInfo, // required for 3ds2
-      shopperIP, // required by some issuers for 3ds2
-      returnUrl: `http://localhost:8080/api/handleShopperRedirect?orderRef=${orderRef}`, // required for 3ds2 redirect flow
-      // special handling for boleto
-      paymentMethod: req.body.paymentMethod.type.includes("boleto")
-        ? {
-            type: "boletobancario_santander",
-          }
-        : req.body.paymentMethod,
-      // Below fields are required for Boleto:
-      socialSecurityNumber: req.body.socialSecurityNumber,
-      shopperName: req.body.shopperName,
-      billingAddress:
-        typeof req.body.billingAddress === "undefined" || Object.keys(req.body.billingAddress).length === 0
-          ? null
-          : req.body.billingAddress,
-      deliveryDate: "2023-12-31T23:00:00.000Z",
-      shopperStatement: "Aceitar o pagamento até 15 dias após o vencimento.Não cobrar juros. Não aceitar o pagamento com cheque",
-      // Below fields are required for Klarna:
-      countryCode: req.body.paymentMethod.type.includes("klarna") ? "DE" : null,
-      shopperReference: "12345",
-      shopperEmail: "youremail@email.com",
-      shopperLocale: "en_US",
-      lineItems: [
-        {
-          quantity: "1",
-          amountExcludingTax: "331",
-          taxPercentage: "2100",
-          description: "Shoes",
-          id: "Item 1",
-          taxAmount: "69",
-          amountIncludingTax: "400",
-        },
-        {
-          quantity: "2",
-          amountExcludingTax: "248",
-          taxPercentage: "2100",
-          description: "Socks",
-          id: "Item 2",
-          taxAmount: "52",
-          amountIncludingTax: "300",
-        },
-      ],
+      reference: orderRef, // required: your Payment Reference
+      returnUrl: `http://localhost:${getPort()}/api/handleShopperRedirect?orderRef=${orderRef}` // set redirect URL required for some payment methods
     });
 
     res.json(response);
@@ -129,24 +66,7 @@ app.post("/api/initiatePayment", async (req, res) => {
   }
 });
 
-app.post("/api/submitAdditionalDetails", async (req, res) => {
-  // Create the payload for submitting payment details
-  const payload = {
-    details: req.body.details,
-    paymentData: req.body.paymentData,
-  };
 
-  try {
-    // Return the response back to client
-    // (for further action handling or presenting result to shopper)
-    const response = await checkout.paymentsDetails(payload);
-
-    res.json(response);
-  } catch (err) {
-    console.error(`Error: ${err.message}, error code: ${err.errorCode}`);
-    res.status(err.statusCode).json(err.message);
-  }
-});
 
 // Handle all redirects from payment type
 app.all("/api/handleShopperRedirect", async (req, res) => {
@@ -201,7 +121,7 @@ app.get("/preview", (req, res) =>
 app.get("/checkout", (req, res) =>
   res.render("checkout", {
     type: req.query.type,
-    clientKey: process.env.CLIENT_KEY,
+    clientKey: process.env.CLIENT_KEY
   })
 );
 
@@ -214,27 +134,48 @@ app.get("/result/:type", (req, res) =>
 
 /* ################# end CLIENT SIDE ENDPOINTS ###################### */
 
+/* ################# WEBHOOK ###################### */
+
+app.post("/api/webhook/notifications", async (req, res) => {
+
+  // YOUR_HMAC_KEY from the Customer Area
+  const hmacKey = process.env.HMAC_KEY;
+  const validator = new hmacValidator()
+  // Notification Request JSON
+  const notificationRequest = req.body;
+  const notificationRequestItems = notificationRequest.notificationItems
+
+  // Handling multiple notificationRequests
+  notificationRequestItems.forEach(function(notificationRequestItem) {
+
+    const notification = notificationRequestItem.NotificationRequestItem
+
+    // Handle the notification
+    if( validator.validateHMAC(notification, hmacKey) ) {
+      // Process the notification based on the eventCode
+        const merchantReference = notification.merchantReference;
+        const eventCode = notification.eventCode;
+        console.log('merchantReference:' + merchantReference + " eventCode:" + eventCode);
+      } else {
+        // Non valid NotificationRequest
+        console.log("Non valid NotificationRequest");
+    }
+});
+
+  res.send('[accepted]')
+});
+
+
+/* ################# end WEBHOOK ###################### */
+
 /* ################# UTILS ###################### */
 
-function findCurrency(type) {
-  switch (type) {
-    case "ach":
-      return "USD";
-    case "wechatpayqr":
-    case "alipay":
-      return "CNY";
-    case "dotpay":
-      return "PLN";
-    case "boletobancario":
-    case "boletobancario_santander":
-      return "BRL";
-    default:
-      return "EUR";
-  }
+function getPort() {
+  return process.env.PORT || 8080;
 }
 
 /* ################# end UTILS ###################### */
 
 // Start server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server started -> http://localhost:${PORT}`));
+app.listen(getPort(), () => console.log(`Server started -> http://localhost:${getPort()}`));
