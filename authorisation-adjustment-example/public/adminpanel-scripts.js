ea08a448ebb21e44f9029a7a6a0554de7cec1d38
Beppe Catanese <gcatanese@yahoo.com>
2023-09-01 13:01:54 +0200

    Add resources

---
+++ authorisation-adjustment-example/public/adminpanel-scripts.js
@@ -0,0 +1,21 @@
+﻿
+// make the form visible
+function show(id) {
+    element = document.getElementById(id);
+    element.removeAttribute("hidden");
+
+    hideOthers(id);
+}
+
+// hide forms excepted form with given id
+function hideOthers(id) {
+    let elements = document.getElementsByClassName("paymentOperationForm");
+
+    for (let i = 0; i < elements.length; i++) {
+        if(elements[i].id != id) {
+            elements[i].setAttribute("hidden", "hidden");
+        }
+    }
+}
+
+
