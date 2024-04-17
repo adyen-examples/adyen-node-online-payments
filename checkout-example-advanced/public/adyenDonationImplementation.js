5b292f0aba06f49427af3d491ba51dd3fc868ce1
Beppe Catanese <gcatanese@yahoo.com>
2024-04-17 16:54:25 +0200

    Add client-side

---
+++ checkout-example-advanced/public/adyenDonationImplementation.js
@@ -0,0 +1,77 @@
+const clientKey = document.getElementById("clientKey").innerHTML;
+
+// initialise Giving
+async function initGivingComponent() {
+  try {
+
+    // get active campaign
+    const donationCampaign = await callServer("/api/getActiveDonationCampaign");
+
+    if(donationCampaign) {
+      // remove id (to re-use object during configuration of the component)
+      delete donationCampaign.id;
+
+      // Create the configuration object
+      const donationConfig = {
+        ...donationCampaign,
+        showCancelButton: true,
+        onDonate: handleOnDonate,
+        onCancel: handleOnCancel
+      };
+
+      const checkout = await new AdyenCheckout(
+        {
+          clientKey,
+          environment: "test",
+        }
+      );
+
+      const donation = checkout.create('donation', donationConfig).mount('#donation-container');
+    }
+
+  } catch (error) {
+    console.error(error);
+    alert("Error occurred. Look at console for details");
+  }
+}
+
+// onDonate handler
+async function handleOnDonate(state, component) {
+  console.log("handleOnDonate")
+
+  if (state.isValid) {
+    // make donation (passing the amount chosen by the shopper)
+    const donation = await callServer("/api/donations", state.data);
+
+    switch (donation.status) {
+      case "completed":
+        window.location.href = "/result/donated";
+        break;
+      default:
+        window.location.href = "/result/error";
+        break;
+    }
+  }
+}
+
+// onCancel handler
+async function handleOnCancel(state, component) {
+  console.log("handleOnCancel")
+  // hide component 
+  document.getElementById('donation-container').style.display = 'none';
+}
+
+// Calls your server endpoints
+async function callServer(url, data) {
+  const res = await fetch(url, {
+    method: "POST",
+    body: data ? JSON.stringify(data) : "",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return await res.json();
+}
+
+initGivingComponent();
