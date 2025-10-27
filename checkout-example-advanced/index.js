ce627386f8f87f75c845d86c2a62ba9fb412cf47
saikotaa <saisrinivasa.kota@aa.com>
2025-10-27 17:04:06 -0500

    add: export

--- checkout-example-advanced/index.js
+++ checkout-example-advanced/index.js
@@ -310,3 +310,5 @@ function findCurrency(type) {
 // Start server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started -> http://localhost:${PORT}`));
+
+module.exports = app;
