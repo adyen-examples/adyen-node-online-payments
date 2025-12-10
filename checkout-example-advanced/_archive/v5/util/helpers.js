2c8e3ce88a6ca9212ae19bdd50ef92d5cc0500ab
ShopskaSalad <akinkamilov@gmail.com>
2025-12-10 11:08:13 +0300

    RELEASE / delete all files but checkout

--- checkout-example-advanced/_archive/v5/util/helpers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ifeq: function(a, b, options) {
-    if (a === b) {
-      return options.fn(this);
-    }
-    return options.inverse(this);
-  }
-};
--- subscription-example/util/helpers.js
+++
@@ -1,9 +0,0 @@
-// Handlebar helper to implement IF EQUAL function 
-module.exports = {
-  ifeq: function(a, b, options) {
-    if (a === b) {
-      return options.fn(this);
-    }
-    return options.inverse(this);
-  }
-};
